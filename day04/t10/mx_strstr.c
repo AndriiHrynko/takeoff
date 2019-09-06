0544cccd7f5f8eecc552ddafc02e6be9866ae28c
Andrii <andrii@MacBook-Pro-AndriiH.local>
2019-09-06 10:25:21 +0300

    day05_to_t02

--- day04/t10/mx_strstr.c
+++ day04/t10/mx_strstr.c
@@ -6,9 +6,19 @@ int mx_strlen(const char *s);
 int mx_strncmp(const char *s1, const char *s2, int n);
 
 char *mx_strstr(const char *s1, const char *s2) {
-    
+
+    int n = mx_strlen(s2);
+    // char *crop_s1;
     int i = 0;
-    int j;
+    int j = 0;
+/*
+    while(*s1){
+        crop_s1 = mx_strchr(s1, s2[0]);
+        if (mx_strncmp(crop_s1, s2, n) == 0)
+            return crop_s1;
+        s1++;
+    }
+*/    
 
     while(s1[i]){
         j = 0;
@@ -19,6 +29,7 @@ char *mx_strstr(const char *s1, const char *s2) {
         }
         i++;
     }
+
     return 0;
 }
 /*
@@ -27,8 +38,8 @@ int main(){
     char *s1 = "abc abd";
     char *s2 = "abd";
 
-    printf("mx_strstr %s\n", mx_strstr(s1, s2));
-    printf("strstr %s\n", strstr(s1, s2));
+    printf("res mx_strstr %s\n", mx_strstr(s1, s2));
+    printf("res strstr %s\n", strstr(s1, s2));
 
     return 0;
 }
--- day04/t11/mx_count_substr.c
+++ day04/t11/mx_count_substr.c
@@ -1,14 +1,15 @@
-// #include <stdio.h>
+#include <stdio.h>
 
 char *mx_strstr(const char *s1, const char *s2);
 int mx_strlen(const char *s);
 int mx_strncmp(const char *s1, const char *s2, int n);
-char *mx_strstr(const char *s1, const char *s2);
+char *mx_strchr(const char *s, int c);
 
 int mx_count_substr(const char *str, const char *sub){
     int i = 0;
 	int j = 0;
     int counter = 0;
+    int n = mx_strlen(sub);
 
 	while (str[i]) {
 		j = 0;
@@ -19,11 +20,22 @@ int mx_count_substr(const char *str, const char *sub){
 		}
 		i++;
 	}
+/*
+    char *crop_str;
+
+    while(*str){
+        crop_str = mx_strstr(str, sub);
+        printf("crop_str %s\n", crop_str);
+        if (mx_strncmp(crop_str, sub, n) == 0)
+            counter++;
+        str++;
+    }
+*/
 	return counter;
 }
 /*
 int main(){
-    char *str = "yo, yo, yo Neo";
+    char *str = "yo, yo, yo Neo yo    yo";
     char *sub = "yo";
     printf("%d\n", mx_count_substr(str, sub));
     return 0;
